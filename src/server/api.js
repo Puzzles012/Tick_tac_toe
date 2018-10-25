8d9a8fb25be487ec53946ab54629b7dd3df94881
Olafur <olafurao16@ru.is>
2018-10-25 17:45:51 +0000

    NO

--- src/server/api.js
+++ src/server/api.js
@@ -2,14 +2,16 @@
 const express = require("express");
 const app = express();
 const game = require("../logic/ttt");
+const index = require(".../dist/index")
 app.get("/ttt/:player", (req, res) => {  
     var player = game.playerTurn(req.params.player);
    
     res.send({game: player});
-    res.status(200);
- 
-   
-   
+    res.status(200); 
 });
+
+app.get("/dist/index", (req, res) => {
+	res.send(index);
+})
 
 module.exports = app;
