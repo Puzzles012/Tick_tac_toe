c9f0f02e2ec394edcbcd4529f66edf351a5afdf4
Olafur <olafurao16@ru.is>
2018-10-25 18:07:31 +0000

    BLÖÖÖRGHh

--- src/server/api.js
+++ src/server/api.js
@@ -2,19 +2,16 @@
 const express = require("express");
 const app = express();
 const game = require("../logic/ttt");
-const index = require("../../dist/index");
-const page = await browser.newPage();
+const index = require("../dist/index");
+
 app.get("/ttt/:player", (req, res) => {  
-    var player = game.playerTurn(req.params.player);
    
-    res.send({game: player});
+    res.send(index);
     res.status(200); 
 });
 
-app.get("/dist/index", async (req, res) => {
-	var whatever = await page.goto("/dist/index.html");
-	res.send(whatever);
-	res.status(200);
-})
+
+
+
 
 module.exports = app;
