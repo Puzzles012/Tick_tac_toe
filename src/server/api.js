0aa33ab76e00d2b169b9b6a932e0bff8e8b42634
fjola17 <34029285+fjola17@users.noreply.github.com>
2018-10-26 11:48:56 +0000

    basic look of the api... doesn't work

--- src/server/api.js
+++ src/server/api.js
@@ -1,15 +1,18 @@
 // src/api.js
 const express = require("express");
 const app = express();
+const index = require("../client/index");
 const game = require("../logic/ttt");
 app.get("/ttt/:player", (req, res) => {  
     var player = game.playerTurn(req.params.player);
    
     res.send({game: player});
     res.status(200);
- 
-   
-   
+});
+app.get(":displayBoard" ,(req, res) => {
+    var component = index.component(req.param.displayBoard);
+    res.send({component});
+    res.status(200);
 });
 
 module.exports = app;
