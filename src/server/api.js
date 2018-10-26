c3260b21efc18f29b2f2837770d70e87abad0070
Olafur <olafurao16@ru.is>
2018-10-26 08:08:50 +0000

    fixing git

--- src/server/api.js
+++ src/server/api.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const game = require("../logic/ttt");
-const index = require("../dist/index");
+const index = require("../../dist/index.html");
 
 app.get("/ttt/index", (req, res) => {  
     //var player = game.playerTurn(req.params.player);
